4474dc318390d38689988f397b61ba711cb07fd5
Sandro Kossel <sandro.kossel@ergon.ch>
2024-04-21 22:29:38 +0200

    create first api reponse and show it at the website

--- waehrungsrechner/app.py
+++ waehrungsrechner/app.py
@@ -1,17 +1,71 @@
 from flask import Flask, render_template
+import request
+import os
+import sys
+import time
+
+
+import requests
 import json
 
 app = Flask(__name__)
 
-# Lade die JSON-Daten aus einer Datei
 
-with open('currency_conversion.json', 'r') as file:
-    data = json.loads(file.read())
+def restartprogram():
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
+
+def main():
+    # Hauptteil Ihres Programms hier einfügen
+    print("Das Programm wird in einer Stunde neu gestartet.")
+
+    # Verzögerung von einer Stunde
+    time.sleep(10)
+
+    # Nach einer Stunde das Programm neu starten
+    restartprogram()
 
 @app.route('/')
 def index():
+    data = request.getDataOfApiRequest()
     print(data)
     return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run()
+    main()
+
+
+
+
+from flask_apscheduler import APScheduler
+app = Flask(__name__)
+scheduler = APScheduler()
+
+# Konfiguriere den Scheduler
+scheduler.api_enabled = True
+scheduler.init_app(app)
+
+# Starte den Scheduler
+scheduler.start()
+
+
+def hourly_job():
+    with app.app_context():
+        index()
+
+
+# Füge den Job zum Scheduler hinzu
+scheduler.add_job(id='hourly_job', func=hourly_job, trigger='interval', seconds=5)
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+
+
+
+
+
--- waehrungsrechner/request.py
+++ waehrungsrechner/request.py
@@ -1,21 +1,18 @@
 import requests
 import json
 
-import schedule
 
-api_url = 'https://api.api-ninjas.com/v1/convertcurrency?want=EUR&have=USD&amount=5000'
+api_url = 'https://api.api-ninjas.com/v1/convertcurrency?want=CHF&have=GBP&amount=1000'
 api_key = 'qTRfOwwc/jXet0zOlGIGvg==f47WreTpyZOG1nM4'
 
-response = requests.get(api_url, headers={'X-Api-Key': api_key})
+def getDataOfApiRequest():
+    response = requests.get(api_url, headers={'X-Api-Key': api_key})
 
-if response.status_code == requests.codes.ok:
-    data = response.json()
+    if response.status_code == requests.codes.ok:
+        data = response.json()
+        print("JSON mit akutellen Daten wurde erfolgreich ausgelesen")
+        return data
 
-    # Speichere die JSON-Antwort in einer Datei
-    with open('currency_conversion.json', 'w') as file:
-        json.dump(data, file, indent=4)
-
-    print("Die Währungsumrechnung wurde erfolgreich in der Datei 'currency_conversion.json' gespeichert.")
-else:
-    print("Fehler:", response.status_code, response.text)
+    else:
+        print("Fehler:", response.status_code, response.text)
 
